ac74ea3e544e876ce1ba6e7e3667872bb2ddca3f
Dennis McMahon <mcmdennis@gmail.com>
2025-07-30 13:29:17 +1000

    app styles improt

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../components/Layout';
-import '@/styles/globals.css';
+import '../styles/globals.css';
 
 
 
