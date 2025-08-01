973e9dd9272ade4901b8c09788889d5d88dba0af
Dennis McMahon <mcmdennis@gmail.com>
2025-08-02 05:54:08 +1000

    haversine fucntion uopdate

--- backend/utils/haversine.py
+++ backend/utils/haversine.py
@@ -1,6 +1,6 @@
 from math import radians, sin, cos, sqrt, atan2
 
-def haversine_distance(lat1, lng1, lat2, lng2):
+def haversine(lat1, lng1, lat2, lng2):
     R = 6371.0  # Earth radius in kilometers
     dlat = radians(lat2 - lat1)
     dlng = radians(lng2 - lng1)
